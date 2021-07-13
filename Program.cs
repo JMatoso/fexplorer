1d5def972e3b26bbff2f43614a0df6f3197135fe
José Matoso <jos3matosoj@gmail.com>
2021-07-13 15:10:06 +0100

    App

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fexplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
